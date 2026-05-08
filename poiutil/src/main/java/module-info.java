e660966baac36737dd550a8fb7fbe21294f3389c
WildeTechSolutions <65636589+WildeTechSolutions@users.noreply.github.com>
2026-05-08 15:59:29 -0600

    Create module-info.java

---
+++ poiutil/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.thomaswilde.documents.ms.poi {
+    
+	requires java.desktop;
+	requires org.apache.xmlbeans;
+	requires org.apache.commons.io;
+	requires org.slf4j;
+	requires org.apache.poi.poi;
+	requires org.apache.poi.ooxml;
+	requires org.apache.poi.scratchpad;
+    requires java.sql;
+
+
+    exports com.thomaswilde.documents.ms.poi;
+}
